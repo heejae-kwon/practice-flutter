028b0f8127d5c9a94ebdb65561331166ff83ad52
권희재 <hjkwon93@fonetech.co.kr>
2021-12-23 10:04:03 +0900

    first commit

---
+++ contact/lib/main1.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+/*void main() {
+  runApp(const MyApp());
+}
+*/
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:  Scaffold(
+        appBar: AppBar(
+          title: Text('앱임'),
+        ),
+        body: Text('안녕'),
+        bottomNavigationBar:  BottomAppBar(
+          child: SizedBox(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
+
+
