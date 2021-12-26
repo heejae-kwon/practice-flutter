cc5f689693571a35e854cafc4c02281710271cdd
Heejae Kwon <sfhy33@gmail.com>
2021-12-26 21:30:12 +0900

    4강 숙제까지

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +16,38 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('앱임'),
         ),
-        body: ElevatedButton(
-          child: Text('글자'),
-          onPressed: (){},
-          style: ButtonStyle(),
-        )
+        body: Container(
+          height: 150,
+          color: Colors.blue,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Container(
+               child: Image.asset('image.jpg', width: 150,),
+                padding: EdgeInsets.all(10),
+              ),
+             SizedBox(
+               width: 300, //퍼센트
+               child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함'),
+                  Text('성당구 행당동 * 끌올 10분 전'),
+                  Text('210,000원'),
+                  Text('하트레이아웃'),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.end,
+                   children: [
+                     Icon(Icons.favorite),
+                     Text('4')
+                   ],
+                 )
+               ],
+             )
+             ),
+            ],
+          ),
+        ),
       )
     );
   }
